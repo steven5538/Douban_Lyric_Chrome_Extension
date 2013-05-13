73f26a6fefac008941d4b0e4434cb0b6f489e848
steven5538 <steven5538@gmail.com>
2013-05-13 17:51:17 +0800

    release

--- douban.js
+++ douban.js
@@ -6,19 +6,21 @@ artist_name = '' ;
 get_url = false ;
 
 lyric.id = "lyric_div" ;
+$(lyric).addClass('overlay') ;
 $(lyric).css({
 	'border':'1px soild' ,
-	'width':'525px' ,
-	'height':'150px' ,
-	'zIndex':'999' ,
-	'position':'absolute' ,
-	'left':'47%' ,
-	'top':'50%' ,
-	'overflow-y':'auto'
+	'width':'510px' ,
+	'height': document.body.clientHeight - 480 ,
+	'margin-top':'255px' ,
+	'overflow-y':'auto' ,
+	'-webkit-box-shadow':'0 0 20px rgba(0,0,0,0.2)' ,
+	'background':'#FFF' ,
+	'display':'block'
 }) ;
-$('body').append(lyric) ;
+$('#fm-player').append(lyric) ;
 
 song_name = $('title').html().split(' - ')[0] ;
+console.log(document.body.clientHeight) ;
 
 $('title').timer({
 	delay: 5000 ,
@@ -43,7 +45,6 @@ $('title').timer({
 		}
 	}
 }) ;
-
 function getLyric()
 {
 	$.get('http://translate.google.com.tw/translate_a/t?client=t&hl=zh-TW&sl=zh-CN&tl=zh-TW&ie=UTF-8&oe=UTF-8&multires=1&otf=1&pc=1&ssel=3&tsel=3&sc=1&q=' + song_name + ' ; ' + artist_name , function(data){
@@ -53,8 +54,8 @@ function getLyric()
 		song = translate_name[0].toLowerCase().replace(' ' , '') ;
 		artist = translate_name[1].toLowerCase() ;
 		
-		console.log(song) ;
-		console.log(artist) ;
+		//console.log(song) ;
+		//console.log(artist) ;
 		
 		$.get('http://mojim.com/' + song + '.html?t3' , function(data){
 			$.each(data.split('<td Class="iA"><a Href="') , function(index , value) {
@@ -64,11 +65,9 @@ function getLyric()
 					
 					if (data.search(song) > 0 && data.search(artist) > 0 && data.search('歌詞') > 0)
 					{
-						console.log("in : " + data) ;
 						url = data.split('" title="')[0] ;
 						if (url.split('x').length == 3 && url.substring(url.length - 3 , url.length) == 'htm')
 						{
-							console.log(url) ;
 							lyric_url = url ;
 							get_url = true ;
 							return false ;
@@ -81,15 +80,14 @@ function getLyric()
 			}) ;
 			
 			if (!get_url)
-				$('#lyric_div').append('Sorry , 404 ...') ;
+				$('#lyric_div').append('Sorry , Can\'t Found ...') ;
 			else
 			{
 				$.get('http://mojim.com' + lyric_url , function(data){
-					console.log(lyric_url) ;
+					//console.log(lyric_url) ;
 					$('#lyric_div').append(data.split('</dt><dd><br />')[1].split('</dl>')[0]) ;
 				}) ;
 			}
-		}) ;
-		
+		}) ;		
 	}) ;
 }
