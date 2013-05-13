12c3f85c6c7b4d36ebf94bb7a9f23d81d6ceed18
steven5538 <steven5538@gmail.com>
2013-05-13 15:42:34 +0800

    v1.0

--- douban.js
+++ douban.js
@@ -1,6 +1,7 @@
 ﻿lyric = document.createElement("div") ;
 lyric_url = '' ;
 song_name = '' ;
+artist_name = '' ;
 
 get_url = false ;
 
@@ -27,6 +28,14 @@ $('title').timer({
 		if (song_name != new_song_name)
 		{
 			$('#lyric_div').empty() ;
+			
+			script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.innerHTML = "$('body').attr('artist', FM.getCurrentSongInfo().artistName);";
+			document.head.appendChild(script);
+			document.head.removeChild(script);
+			artist_name = $('body').attr('artist');
+					
 			lyric_url = '' ;
 			get_url = false ;
 			song_name = new_song_name ;
@@ -37,37 +46,49 @@ $('title').timer({
 
 function getLyric()
 {
-	$.get('http://translate.google.com.tw/translate_a/t?client=t&hl=zh-TW&sl=zh-CN&tl=zh-TW&ie=UTF-8&oe=UTF-8&multires=1&otf=1&pc=1&ssel=3&tsel=3&sc=1&q=' + song_name , function(data){
+	$.get('http://translate.google.com.tw/translate_a/t?client=t&hl=zh-TW&sl=zh-CN&tl=zh-TW&ie=UTF-8&oe=UTF-8&multires=1&otf=1&pc=1&ssel=3&tsel=3&sc=1&q=' + song_name + ' ; ' + artist_name , function(data){
 		
-		translate_name = data.split('[[["')[1].split('"')[0] ;
+		translate_name = data.split('[[["')[1].split('"')[0].split(' ; ') ;
 		
-		console.log(translate_name) ;
+		song = translate_name[0].toLowerCase().replace(' ' , '') ;
+		artist = translate_name[1].toLowerCase() ;
 		
-		$.get('http://mojim.com/' + translate_name + '.html?t3' , function(data){
+		console.log(song) ;
+		console.log(artist) ;
+		
+		$.get('http://mojim.com/' + song + '.html?t3' , function(data){
 			$.each(data.split('<td Class="iA"><a Href="') , function(index , value) {
 				if (index != 0)
 				{
-					$.each(data.split('<td Class="iA"><a Href="')[index].split('" Title="') , function(index , value) {
-						if (index == 0)
+					data = value.toLowerCase() ;
+					
+					if (data.search(song) > 0 && data.search(artist) > 0 && data.search('歌詞') > 0)
+					{
+						console.log("in : " + data) ;
+						url = data.split('" title="')[0] ;
+						if (url.split('x').length == 3 && url.substring(url.length - 3 , url.length) == 'htm')
 						{
-							if (value.split('x').length == 3)
-							{
-									lyric_url = value ;
-									get_url = true ;
-									return false ;
-							}
+							console.log(url) ;
+							lyric_url = url ;
+							get_url = true ;
+							return false ;
 						}
-					}) ;
+					}
 				}
 				
 				if (get_url)
 					return false ;
 			}) ;
 			
-			$.get('http://mojim.com' + lyric_url , function(data){
-				console.log(lyric_url) ;
-				$('#lyric_div').append(data.split('</dt><dd><br />')[1].split('</dl>')[0]) ;
-			}) ;
+			if (!get_url)
+				$('#lyric_div').append('Sorry , 404 ...') ;
+			else
+			{
+				$.get('http://mojim.com' + lyric_url , function(data){
+					console.log(lyric_url) ;
+					$('#lyric_div').append(data.split('</dt><dd><br />')[1].split('</dl>')[0]) ;
+				}) ;
+			}
 		}) ;
 		
 	}) ;
